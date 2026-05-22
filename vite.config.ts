f35a20bf09433c149697db73184c155c305c89a9
Dewansh <dewansh.mishra@mindbowser.com>
2026-05-23 00:02:27 +0530

    chore: update deployment path and router basename from /mobile to /clinic

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/health-telematix/mobile/',
+  base: '/health-telematix/clinic/',
   plugins: [react()],
   resolve: {
     alias: {
